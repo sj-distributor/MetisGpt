aa35db10b9bc55c0b83c406fef57324798a3ec46
Eddy Ma <ocean_ltd@hotmail.com>
2023-04-25 20:57:07 -0700

    reduce batch

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
@@ -71,13 +71,13 @@ def parse_args():
     parser.add_argument(
         "--per_device_train_batch_size",
         type=int,
-        default=12,
+        default=8,
         help="Batch size (per device) for the training dataloader.",
     )
     parser.add_argument(
         "--per_device_eval_batch_size",
         type=int,
-        default=12,
+        default=8,
         help="Batch size (per device) for the evaluation dataloader.",
     )
     parser.add_argument(
